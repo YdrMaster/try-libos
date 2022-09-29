a08b07f610afad5f6f45a883c729342d31afe482
YdrMaster <ydrml@hotmail.com>
2022-09-29 14:28:42 +0800

    init
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ libos/src/lib.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![feature(naked_functions, asm_sym, asm_const)]
+#![feature(linkage)]
+
+use core::fmt::{self, Write};
+use sbi_rt::*;
+
+#[linkage = "weak"]
+#[no_mangle]
+fn app_main() {
+    panic!()
+}
+
+#[link_section = ".text.entry"]
+#[no_mangle]
+#[naked]
+unsafe extern "C" fn _start() -> ! {
+    const STACK_SIZE: usize = 4096;
+
+    #[link_section = ".bss.uninit"]
+    static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
+
+    core::arch::asm!(
+        "la sp, {stack} + {stack_size}",
+        "j  {main}",
+        stack_size = const STACK_SIZE,
+        stack      =   sym STACK,
+        main       =   sym rust_main,
+        options(noreturn),
+    )
+}
+
+extern "C" fn rust_main() -> ! {
+    app_main();
+    system_reset(RESET_TYPE_SHUTDOWN, RESET_REASON_NO_REASON);
+    unreachable!()
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    crate::println!("{info}");
+    system_reset(RESET_TYPE_SHUTDOWN, RESET_REASON_SYSTEM_FAILURE);
+    unreachable!()
+}
+
+struct Console;
+
+impl Write for Console {
+    #[inline]
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.bytes() {
+            #[allow(deprecated)]
+            sbi_rt::legacy::console_putchar(c as _);
+        }
+        Ok(())
+    }
+}
+
+#[doc(hidden)]
+#[inline]
+pub fn _print(args: fmt::Arguments) {
+    let _ = Console.write_fmt(args);
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::_print(core::format_args!($($arg)*));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => {{
+        $crate::_print(core::format_args!($($arg)*));
+        $crate::println!();
+    }}
+}
---
+++ xtask/src/main.rs
@@ -0,0 +1,99 @@
+#[macro_use]
+extern crate clap;
+
+use clap::Parser;
+use command_ext::{BinUtil, Cargo, CommandExt, Qemu};
+use once_cell::sync::Lazy;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+const TARGET_ARCH: &str = "riscv64gc-unknown-none-elf";
+
+static PROJECT: Lazy<&'static Path> =
+    Lazy::new(|| Path::new(std::env!("CARGO_MANIFEST_DIR")).parent().unwrap());
+
+static TARGET: Lazy<PathBuf> = Lazy::new(|| PROJECT.join("target").join(TARGET_ARCH));
+
+#[derive(Parser)]
+#[clap(name = "perf-playground")]
+#[clap(version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Make(BuildArgs),
+    Asm(BuildArgs),
+    Qemu(BuildArgs),
+}
+
+fn main() {
+    use Commands::*;
+    match Cli::parse().command {
+        Make(args) => args.make(),
+        Asm(args) => args.asm(),
+        Qemu(args) => args.qemu(),
+    }
+}
+
+#[derive(Args, Default)]
+struct BuildArgs {
+    /// app
+    #[clap(long)]
+    app: String,
+    /// log level
+    #[clap(long)]
+    log: Option<String>,
+}
+
+impl BuildArgs {
+    fn make(&self) {
+        Cargo::build()
+            .package(&self.app)
+            .optional(&self.log, |cargo, level| {
+                cargo.env("LOG", level);
+            })
+            .release()
+            .target(TARGET_ARCH)
+            .invoke();
+    }
+
+    fn asm(&self) {
+        self.make();
+        let elf = TARGET.join("release").join(&self.app);
+        let out = PROJECT.join("kernel.asm");
+        fs::write(out, BinUtil::objdump().arg(elf).arg("-d").output().stdout).unwrap();
+    }
+
+    fn qemu(&self) {
+        self.make();
+        let elf = TARGET.join("release").join(&self.app);
+        Qemu::system("riscv64")
+            .args(["-machine", "virt"])
+            .arg("-kernel")
+            .arg(objcopy(elf, true))
+            .args(["-smp", "1"])
+            .args(["-serial", "mon:stdio"])
+            .args(["-m", "2G"])
+            .arg("-nographic")
+            .invoke();
+    }
+}
+
+fn objcopy(elf: impl AsRef<Path>, binary: bool) -> PathBuf {
+    let elf = elf.as_ref();
+    let bin = elf.with_extension("bin");
+    BinUtil::objcopy()
+        .arg(elf)
+        .arg("--strip-all")
+        .conditional(binary, |binutil| {
+            binutil.args(["-O", "binary"]);
+        })
+        .arg(&bin)
+        .invoke();
+    bin
+}
